c2f4b052ce661d89a8dcdd53af7a055582f22134
hmin01 <goals1003@gmail.com>
2022-01-06 18:09:40 +0900

    2021.01.06. - Modify shell script to upload source code for lambda function - Modify buildspec file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,37 @@
 #!/bin/bash
-aws --version
+
+# Install jq
+sudo curl -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o /usr/local/bin/jq
+sudo chmod a+x /usr/local/bin/jq
+
+# Extract the lambda alias version
+aws lambda get-alias --function-name $DEPLOY_LAMBDA_NAME --name $DEPLOY_LAMBDA_ALIAS > lambda-alias.json
+# Extract the current version for this lambda alias
+CURRENT_LAMBDA_ALIAS_VERSION = $(cat lambda-alias.json | jq -r '.FunctionVersion')
+
+# Compress source code
+zip -r ../dist/source.zip ../src/*
+
+# Upload source for lambda function
+aws lambda update-function-code --function-name $DEPLOY_LAMBDA_NAME --zip-file fileb://dist/source.zip --publish > update-lambda.json
+
+# Extract last version for lambda function
+LATEST_VERSION = $(cat update-lambda.json | jq -r '.Version')
+if [[ $CURRENT_LAMBDA_ALIAS_VERSION -ge $LATEST_VERSION ]]; then
+  exit 0
+fi
+
+# Create appspec file in s3 bucket to create a deployment for AWS CodeDeploy
+cat > appspec.yml <<- EOM
+version: 0.0
+Resources:
+  - deployLambda:
+      Type: AWS::LAMBDA::FUNCTION
+      Properties:
+        Name: "$DEPLOY_LAMBDA_NAME"
+        Alias: "$DEPLOY_LAMBDA_ALIAS"
+        CurrentVersion: "$CURRENT_LAMBDA_ALIAS_VERSION"
+        TargetVersion: "$LATEST_VERSION"
+EOM
+# Upload appspec file in s3 bucket
+aws s3 cp appspec.yml s3://$S3_BUCKET/codeDeploy/appspec.yml
--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print('Hello, world')
+import json
+
+def lambda_handler(event, context):
+  print('Hello, world')
+  # TODO implement
+  return {
+    'statusCode': 200,
+    'body': json.dumps('Hello from Lambda!')
+  }
