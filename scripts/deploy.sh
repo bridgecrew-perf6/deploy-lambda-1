a9406bf9d4d168f56b92d9fd9c34169d6c244379
hmin01 <goals1003@gmail.com>
2022-01-06 18:15:59 +0900

    2021.01.06. - Modify command in shell sciprt

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ sudo chmod a+x /usr/local/bin/jq
 # Extract the lambda alias version
 aws lambda get-alias --function-name $DEPLOY_LAMBDA_NAME --name $DEPLOY_LAMBDA_ALIAS > lambda-alias.json
 # Extract the current version for this lambda alias
-CURRENT_LAMBDA_ALIAS_VERSION = $(cat lambda-alias.json | jq -r '.FunctionVersion')
+CURRENT_LAMBDA_ALIAS_VERSION=$(cat lambda-alias.json | jq -r '.FunctionVersion')
 
 # Compress source code
 mkdir ./dist
@@ -17,7 +17,7 @@ zip -r ../dist/source.zip -i ../src/*
 aws lambda update-function-code --function-name $DEPLOY_LAMBDA_NAME --zip-file fileb://dist/source.zip --publish > update-lambda.json
 
 # Extract last version for lambda function
-LATEST_VERSION = $(cat update-lambda.json | jq -r '.Version')
+LATEST_VERSION=$(cat update-lambda.json | jq -r '.Version')
 if [[ $CURRENT_LAMBDA_ALIAS_VERSION -ge $LATEST_VERSION ]]; then
   exit 0
 fi
