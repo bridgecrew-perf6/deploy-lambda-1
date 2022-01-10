753fa571029e8d0ad588971d188fcc725887c593
hmin01 <goals1003@gmail.com>
2022-01-10 14:40:02 +0900

    2022.01.10. - Modify source code and test code to test build - Modify buildspec file to test build - Add input data file

--- src/testCodeDeployLambda/main.py
+++ src/testCodeDeployLambda/main.py
@@ -1,9 +1,20 @@
 import json
 
 def lambda_handler(event, context):
-  print('Hello, world')
+  print('Process start')
   # TODO implement
+  if 'status' in event.keys():
+    if event['status'] == 'test':
+      return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+      }
+    else:
+      return {
+        'statusCode': 400,
+        'body': json.dumps('Not found status')
+      }
   return {
     'statusCode': 400,
-    'body': json.dumps('Hello from Lambda!')
+    'body': json.dumps('Not found input data')
   }
--- test/testCodeDeployLambda/test.py
+++ test/testCodeDeployLambda/test.py
@@ -1,8 +1,17 @@
+LAMBDA_NAME = "testCodeDeployLambda"
 # Append module path
-import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(os.getcwd() + "/src/testCodeDeployLambda/main.py")))
+import sys, os, pytest
+sys.path.append(os.path.dirname(os.path.abspath(os.path.join(os.getcwd(), "src", LAMBDA_NAME, "main.py"))))
 # Import origin function
 from main import lambda_handler
+# Import json
+import json
 
 def test_lambda_handler():
-  assert lambda_handler({}, '')['statusCode'] < 400
+  # Read json file
+  try:
+    with open(os.path.join(os.getcwd(), "..", LAMBDA_NAME, "input.json")) as jsonFile:
+      data = json.load(jsonFile)
+      assert lambda_handler(data, '')['statusCode'] < 400
+  except: 
+    return pytest.ExitCode.TESTS_FAILED
