3ae160b2b9a3cc1427e32790ceb134e228aeaad0
hmin01 <goals1003@gmail.com>
2022-01-10 10:56:10 +0900

    2022.01.10. - Modify test file - Modify buildspec file

--- test/testCodeDeployLambda/test.py
+++ test/testCodeDeployLambda/test.py
@@ -1,7 +1,7 @@
 # Append module path
 import sys, os
-# sys.path.append(os.path.dirname(os.path.abspath(os.getcwd() + "./src/testCodeDeployLambda/main.py")))
-sys.path.append(os.path.dirname(os.path.abspath("D:\\hmin\\2021\\study\\node\\deploy-lambda\\src\\testCodeDeployLambda\\main.py")))
+sys.path.append(os.path.dirname(os.path.abspath(os.getcwd() + "./src/testCodeDeployLambda/main.py")))
+# sys.path.append(os.path.dirname(os.path.abspath("D:\\hmin\\2021\\study\\node\\deploy-lambda\\src\\testCodeDeployLambda\\main.py")))
 # Import origin function
 from main import lambda_handler
 
