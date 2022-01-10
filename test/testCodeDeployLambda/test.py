7a719ca0f3f2e9c1dae11e5ea92540e023d27043
hmin01 <goals1003@gmail.com>
2022-01-10 15:19:41 +0900

    2022.01.10. - Add gitignore file - Modify test code to test build

--- test/testCodeDeployLambda/test.py
+++ test/testCodeDeployLambda/test.py
@@ -1,17 +1,25 @@
 LAMBDA_NAME = "testCodeDeployLambda"
 # Append module path
-import sys, os, pytest
+import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.join(os.getcwd(), "src", LAMBDA_NAME, "main.py"))))
 # Import origin function
 from main import lambda_handler
-# Import json
-import json
+# Import json, pyttest
+import json, pytest
 
 def test_lambda_handler():
+  # Set input file path
+  iPath = os.path.join(os.getcwd(), "test", LAMBDA_NAME, "input.json")
+
+  # Check input file existance
+  if os.path.exists(iPath) == False:
+    pytest.fail("Not found input file to test build")
+
   # Read json file
-  try:
-    with open(os.path.join(os.getcwd(), "..", LAMBDA_NAME, "input.json")) as jsonFile:
+  with open(os.path.join(os.getcwd(), "test", LAMBDA_NAME, "input.json")) as jsonFile:
+    data = {}
+    try:
       data = json.load(jsonFile)
-      assert lambda_handler(data, '')['statusCode'] < 400
-  except: 
-    return pytest.ExitCode.TESTS_FAILED
+    except:
+      pytest.fail()
+    assert lambda_handler(data, '')['statusCode'] < 400
